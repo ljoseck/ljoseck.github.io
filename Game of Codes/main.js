35d0ed2eab7ee7e87ad22def5d42d80aec9866ac
ljoseck <Louis.joseck@ttu.edu>
2018-02-08 19:35:28 -0600

    Add files via upload

--- Game of Codes/main.js
+++ Game of Codes/main.js
@@ -2,12 +2,13 @@ var globalX = 0;
 var globalY = 0;
 
 
-
+var y = 4;
+var x = 5;
 function draw(){
 	var canvas = document.getElementById("canvas");
 
 	var width = canvas.width = window.innerWidth;
-	var height = canvas.height = window.innerHeight;
+	var height = canvas.height = window.innerHeight*2;
 	// var width = document.getElementById("div1").offsetWidth;
 	// var height = document.getElementById("div1").offsetHeight;
 
@@ -18,8 +19,8 @@ function draw(){
 
 	var ctx = canvas.getContext('2d');
 	
-	var x = 5;
-	var y = 4;
+	x = x + 1;
+	//y = y + 1;
 	var unit = makeSquareGrid(canvas, x, y);
 	addImageToGrid('ground', canvas, 0, 0, unit);
 	for(var i = 0; i < x; i++){
@@ -39,14 +40,15 @@ function draw(){
 	
 	var mainCanvas = document.getElementById("mainCanvas");
 	mainCtx = mainCanvas.getContext('2d');
-	var width = document.getElementById("div1").offsetWidth;
-	var height = document.getElementById("div1").offsetHeight;
+	var width = document.getElementById("sectionleft").offsetWidth;
+	var height = document.getElementById("sectionleft").offsetHeight;
 
 	//alert(height);
 	mainCanvas.width = width;
 	mainCanvas.height = height;
-	mainCtx.drawImage(canvas, globalX, globalY, canvas.width, canvas.height, 0, 0, width + globalX, height + globalY);
-	
+	console.log([width + globalX, (width + globalX)/canvas.width*canvas.height]);
+	mainCtx.drawImage(canvas, globalX, globalY, canvas.width, canvas.height, 0, 0, width + globalX, (width + globalX)/canvas.width*canvas.height);
+	//mainCtx.drawImage(canvas, 0, 0);
 }
 
 function makeGrid(canvas, x, y){
@@ -67,6 +69,7 @@ function makeGridHelper(ctx, x, y, width, height){
 		ctx.lineTo(width,j*height/y);
 		ctx.stroke();
 	}
+	//console.log([width/x, height/y])
 	return[width/x, height/y];
 }
 
