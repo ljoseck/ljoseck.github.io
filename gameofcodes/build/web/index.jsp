86d306362d495c8461ccb97c5da727797f387355
Felix Perez <felix.perez@ttu.edu>
2018-04-13 18:56:14 -0500

    Write JUnit Tests for AccountValidation Class

--- gameofcodes/build/web/index.jsp
+++ gameofcodes/build/web/index.jsp
@@ -11,56 +11,39 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Login</title>
+        <title>Log-In</title>
     </head>
 
     <body>
         
-        <div class='container'>
-            
-            <div class='row'>
-                <div class='text-center'>
-                    <img src="GoC-Logo2.png" alt="Game-Of-Codes" width='400' height='300'>
-                </div>
+        <div class='.container-fluid'>
+            <div class='col-lg-12'>
+                <h1>Welcome to Game of Codes!</h1>
             </div>
-
-            <div class='row'>
-                <div class='col-lg-12' align='center' style="padding-bottom:20px">
-                    <h2>Login</h2>
-                </div>
-            </div>
-
-            <div class="row mt-5">
-                <form action="loginprocess.jsp" method="post"> 
-                    <div class="row" align="center">
-                        <div class='form-group'>
-                            <label for="exampleInputEmail1">Username</label>
-                            <input type="text" id="exampleInputEmail1" name="username" placeholder="Enter Username">
-                        </div>
-                    </div>
-                    <div class="row" align="center">
-                        <div class='form-group'>
-                            <label for="exampleInputPassword1">Password</label>
-                            <input type="password" name="password" placeholder="Enter Password">
-                        </div>
-                    </div>
-                    <div class='row' align="center">
-                        <div class='col-xl-1'>
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-
-
-            <div class='row'>
-                <div class='col-lg-12' align='center'>
-                    <p>Not a user? Sign-Up or Play as a Guest:</p>
-                    <a href="signup.jsp">Sign-Up</a> | <a href="main_menu.html">Play as Guest</a><br> 
-                </div>
-            </div>
-
         </div>
+
+        <p>Please Log-In:</p>
+        <form action="loginprocess.jsp" method="post"> 
+            <table>
+            <tr>
+                <td>Username:</td>
+                <td><input type="text" name="username"/></td>
+                
+            </tr>
+            <tr>
+                <td>Password:</td>
+                <td><input type="password" name="password"/></td>
+                
+            </tr>
+            <tr>
+                <td><input type="submit" value="Log-In"/></td>
+                <td></td>
+            </tr>   
+            </table> 
+        </form>
+            
+        <p>Not a user? Sign-Up or Play as a Guest:</p>
+        <a href="signup.jsp">Sign-Up</a> | <a href="main_menu.html">Play as Guest</a><br> 
 
     </body>
 </html>
--- gameofcodes/src/java/GameOfCodes/AccountValidation.java
+++ gameofcodes/src/java/GameOfCodes/AccountValidation.java
@@ -1,6 +1,8 @@
 /*
  */
 package GameOfCodes;
+import org.junit.*;
+import static org.junit.Assert.*;
 
 /**
  *
@@ -20,4 +22,109 @@ public class AccountValidation {
         }
         return result;
     }
+    
+    @Test
+    public void testNoUsername(){
+        UserAccount test = new UserAccount();
+        test.setClassNumber("1336");
+        test.setEducatorFlag(1);
+        test.setFirstName("Jane");
+        test.setLastName("Doe");
+        test.setLevelsCompleted(0);
+        test.setPassword("password");
+        test.setUsername(null);
+        
+        assertFalse(test.createAccount(test.getUsername(), test.getPassword(), 
+                test.getFirstName(), test.getLastName(),
+                test.getClassNumber(), test.getEducatorFlag()));
+        
+    }
+    
+    @Test
+    public void testNoPassword(){
+        UserAccount test = new UserAccount();
+        test.setClassNumber("1336");
+        test.setEducatorFlag(1);
+        test.setFirstName("Jane");
+        test.setLastName("Doe");
+        test.setLevelsCompleted(0);
+        test.setPassword(null);
+        test.setUsername("user");
+        
+        assertFalse(test.createAccount(test.getUsername(), test.getPassword(), 
+                test.getFirstName(), test.getLastName(),
+                test.getClassNumber(), test.getEducatorFlag()));
+    }
+    
+    @Test
+    public void testNoFirstName(){
+        UserAccount test = new UserAccount();
+        test.setClassNumber("1336");
+        test.setEducatorFlag(1);
+        test.setFirstName(null);
+        test.setLastName("Doe");
+        test.setLevelsCompleted(0);
+        test.setPassword("password");
+        test.setUsername("user");
+        
+        assertFalse(test.createAccount(test.getUsername(), test.getPassword(), 
+                test.getFirstName(), test.getLastName(),
+                test.getClassNumber(), test.getEducatorFlag()));
+    }
+    
+    @Test
+    public void testNoLastName(){
+        UserAccount test = new UserAccount();
+        test.setClassNumber("1336");
+        test.setEducatorFlag(1);
+        test.setFirstName("Jane");
+        test.setLastName(null);
+        test.setLevelsCompleted(0);
+        test.setPassword("password");
+        test.setUsername("user");
+        
+        assertFalse(test.createAccount(test.getUsername(), test.getPassword(), 
+                test.getFirstName(), test.getLastName(),
+                test.getClassNumber(), test.getEducatorFlag()));
+    }
+    
+    /**
+     * Note: for the two tests below, it is assumed that the user Jane Doe 
+     * with the specified account details below does not exist in the database.
+     * If such an account exists, delete the account information an rerun the
+     * JUnit Test.
+     * */
+    @Before
+    public void testSuccessfulValidation(){
+        UserAccount test = new UserAccount();
+        test.setClassNumber("1336");
+        test.setEducatorFlag(1);
+        test.setFirstName("Jane");
+        test.setLastName("Doe");
+        test.setLevelsCompleted(0);
+        test.setPassword("password");
+        test.setUsername("user");
+        
+        assertTrue(test.createAccount(test.getUsername(), test.getPassword(), 
+                test.getFirstName(), test.getLastName(),
+                test.getClassNumber(), test.getEducatorFlag()));
+    }
+    
+    @After
+    public void testUnsuccessfulValidation(){
+        UserAccount test = new UserAccount();
+        test.setClassNumber("1336");
+        test.setEducatorFlag(1);
+        test.setFirstName("Jane");
+        test.setLastName("Doe");
+        test.setLevelsCompleted(0);
+        test.setPassword("password");
+        test.setUsername("user");
+        
+        assertFalse(test.createAccount(test.getUsername(), test.getPassword(), 
+                test.getFirstName(), test.getLastName(),
+                test.getClassNumber(), test.getEducatorFlag()));
+    }
+    
+
 }
