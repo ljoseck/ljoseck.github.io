33b5e620dc06dba84d414446bd047e48cc5fd013
ljoseck <Louis.joseck@ttu.edu>
2018-09-05 14:24:10 -0500

    start animations

--- main.js
+++ main.js
@@ -47,13 +47,33 @@ async function main() {
 	// canvas.fill();
 	// canvas.stroke();
 	//console.log(B(2,[[1,2]]));
-	for(var i = 100; i <= 1000; i++){
+	
+	
+	// var [[x1, y1], [x2, y2]] = [[100, 500], [1000,50]];
+	// //line(x1, y1, x2, y2);
+	// var [x3, y3] = triangulate(.5, [[x1, y1], [x2, y2]]);
+	// canvas.beginPath();
+	// stripComplex([[x1, y1], [x3, y3], [x2, y2]], 50, 50);
+	// canvas.stroke();
+	// canvas.fill();
+	// return;
+	for(var i = 0; i <= 1; i += .0002){
 		clearCanvas();
-		var [[x1, y1], [x2, y2]] = [[100,50], [i,200]];
-		line(x1, y1, x2, y2);
-		var [x3, y3] = rename(1, [[x1, y1], [x2, y2]]);
-		line(x2, y2, x3, y3);
-		await sleep(1);
+		var [[x1, y1], [x2, y2]] = [[100, 500], [1000,50]];
+		//line(x1, y1, x2, y2);
+		var [x3, y3] = triangulate(.5, [[x1, y1], [x2, y2]]);
+		stripComplex([[x1, y1], [x3, y3], [x2, y2]], 50, 50, i+.1);
+		stripComplex([[x1, y1], [x3, y3], [x2, y2]], 50, 50, i+.2);
+		stripComplex([[x1, y1], [x3, y3], [x2, y2]], 50, 50, i+.3);
+		stripComplex([[x1, y1], [x3, y3], [x2, y2]], 50, 50, i+.4);
+		stripComplex([[x1, y1], [x3, y3], [x2, y2]], 50, 50, 1-i-.1);
+		stripComplex([[x1, y1], [x3, y3], [x2, y2]], 50, 50, 1-i-.2);
+		stripComplex([[x1, y1], [x3, y3], [x2, y2]], 50, 50, 1-i-.3);
+		stripComplex([[x1, y1], [x3, y3], [x2, y2]], 50, 50, 1-i-.4);
+		// stripComplex([[x1, y1], [x3, y3], [x2, y2]], 100, 100);
+		// context.bezierCurveTo(x1,x2,)
+		//line(x2, y2, x3, y3);
+		await sleep(100);
 	}
 	
 	
@@ -61,7 +81,16 @@ async function main() {
 	//testB([[1,2],[100,200],[200,100]]);
 }
 
-
+function stripComplex(points, offsetX, offsetY, dist){ // two points
+	BSimple(points);
+	// dist = .1;
+	var subdist = .5;
+	var [point1, point2] = [B(dist, points), B(dist, addComplex(points, offsetX, offsetY))];
+	var point3 = triangulate(subdist, [point1, point2]);
+	BSimple([point1, point3, point2]);
+	BSimple(addComplex(points, offsetX, offsetY));
+	
+}
 function clearCanvas(){
 	canvas.clearRect(0, 0, sizeX, sizeY);
 }
@@ -70,20 +99,34 @@ function sleep(ms){
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function rename(dist, points){//two points
+function triangulate(dist, points){//two points
 	var [[x1, y1], [x2, y2]] = points;
-	return [(x1 + x2)/2 + ((y2 - y1)/(x2 - x1))*dist, (y1 + y2)/2 + ((y2 - y1)/(x2 - x1))*dist];
+	var [x3, y3] = [(x1 + x2)/2, (y1 + y2)/2];
+	// return [x3, y3]
+	// console.log(x1, y1, x3, y3, [x3 + y3 - y1, y3 + x3 - x1]);
+	return [x3 + dist*(y3 - y1), y3 - dist*(x3 - x1)];
 }
 
 
+function BSimple(points){ // 3 points
+	canvas.beginPath();
+	var [[x1, y1], [x2, y2], [x3, y3]] = points;
+	canvas.moveTo(x1, y1);
+	
+	canvas.quadraticCurveTo(x2, y2, x3, y3);
+	canvas.stroke();
+}
+
 function testB(points){
 	var [x1, y1] = B(0, points);
-	for(var i = .05; i <= 1; i += .05){
+	for(var i = .1; i <= 1.01; i += .1){
+		i > .9 ? console.log(i) : "";
 		var [x2, y2] = B(i, points);
 		line(x1, y1, x2, y2);
 		[x1, y1] = [x2, y2];
 	}
 }
+
 function BComplex(points){
 	var [x1, y1] = B(0, points);
 	for(var i = .05; i <= 1; i += .05){
@@ -117,15 +160,23 @@ function B(t, points){
 }
 
 function multiply(i, list){
-	result = [];
+	var result = [];
 	list.forEach(x => result.push(i * x));
 	return result;
 }
 
 function add(list1, list2){
-	result = [];
+	var result = [];
 	for(var i = 0; i < list1.length; i++){
 		result.push(list1[i] + list2[i]);
+	}
+	return result;
+}
+
+function addComplex(list1, x, y){
+	var result = [];
+	for(var i = 0; i < list1.length; i++){
+		result.push(add(list1[i], [x, y]));
 	}
 	return result;
 }
