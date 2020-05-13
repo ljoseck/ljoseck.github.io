167b4576bd15857ac392bd4872792bebe89aa1b3
ljoseck <louis.joseck@ttu.edu>
2020-05-13 13:35:48 -0500

    add lost gems

--- programs/main.js
+++ programs/main.js
@@ -1,5 +1,6 @@
 var cloneSets = Ext.create('Ext.data.Store', {  data: [
 
+          {'1':"Lost Gems of Condor", '2':"JavaScript", '3':"2020", '4':"Present", '5': "Personal Project",'6':4718},
           {'1':"3D Renderer", '2':"JavaScript", '3':"2018", '4':"2018", '5': "Personal Project",'6':404},
           {'1':"Game Of Codes", '2':"JavaScript", '3':"2018", '4':"2018", '5': "School Project",'6':1334},
           {'1':"File System", '2':"C", '3':"2016", '4':"2016", '5': "School Project",'6':426},
@@ -13,6 +14,8 @@ var cloneSets = Ext.create('Ext.data.Store', {  data: [
 
 function projectRenderer(val) {
     if (val != null) {
+		if(val == "Lost Gems of Condor")
+			return "<a href='https://lostgemsofcondor.itch.io/lost-gems-of-condor'>" + val + "</a>";
 		return "<a href='./" + val.replace(/ /g,"") + "/index.html'>" + val + "</a>";
         //return "<a href=\'xCloneSet" + val + ".html'  target='_blank'>CloneSet" + val + "</a>"; open new tab for cloneSet
     }
